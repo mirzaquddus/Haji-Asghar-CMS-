c1945e02718f83c894841e2b9700102f33120ae6
mirzaquddus <mirzaquddus00@gmail.com>
2025-03-18 21:51:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website Loaded Successfully!");
 });
+function addToCart(productName, productPrice) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push({ name: productName, price: productPrice });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(productName + " added to cart!");
+}
