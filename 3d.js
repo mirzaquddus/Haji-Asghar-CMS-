c3e1df4ff01d5314e7f1e3a79005f79fc11bf1bd
mirzaquddus <mirzaquddus00@gmail.com>
2025-03-18 22:05:58 +0500

    Delete 3d.js

--- 3d.js
+++
@@ -1,6 +0,0 @@
-function addToCart(productName, productPrice) {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    cart.push({ name: productName, price: productPrice });
-    localStorage.setItem("cart", JSON.stringify(cart));
-    alert(productName + " added to cart!");
-}
